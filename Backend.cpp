0b51fab427ef46a5b2ebd0511e80cfdc8fc2d8ff
mielevoevo <mielevoevo@outlook.com>
2025-11-25 16:49:58 +0700

    Co-authored-by: k4hfie <k4hfie@users.noreply.github.com>

---
+++ Backend.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <math.h>
+#include <bits//stdc++.h>
+
+using namespace std;
+
+//Vector Class
+class VectorType
+{
+    public:
+    //Parameters
+        int x_magnitude;
+        int y_magnitude;
+        int z_magnitude;
+    
+    //Contructor, Standard Vector
+    VectorType();
+    VectorType(int x_magnitude, int y_magnitude, int z_magnitude)
+    {
+        this->x_magnitude = x_magnitude;
+        this->y_magnitude = y_magnitude;
+        this->z_magnitude = z_magnitude;    
+    }
+};
+
+//DEBUGGING AND TESTING
+int main()
+{
+    int x,y,z;
+
+    VectorType vector1(x, y, z);
+    
+    cout << vector1.x_magnitude << "\n";
+    cout << vector1.y_magnitude << "\n";
+    cout << vector1.z_magnitude << "\n";
+}
---
+++ Frontend.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+Print()
+{
+
+}
+int main()
+{
+    //Title Screen
+    cout <<"==========================" << "\n";
+    cout <<"VECTOR AND MATRIX TOOL KIT" << "\n";
+    cout <<"  Made by Kahfie and Evo  " << "\n";
+    cout <<"==========================" << "\n";
+
+    //Selection
+    cout << "Choose the Operation Type!" << "\n"
+    cout << 
+
+    //Operation Types
+    cout << "Choose the Operation Type!" << "\n"
+
+    
+    
+}
